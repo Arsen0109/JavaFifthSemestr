baf909b9fd84f3d32681edcab182d8ac595ef17b
Arsen0109 <balasarseniy777@gmail.com>
2022-09-22 11:33:03 +0300

    checking user input

--- src/lab1java/Lab1.java
+++ src/lab1java/Lab1.java
@@ -1,30 +1,50 @@
 package lab1java;
-
+/**
+ * C2 = 0; operation1: +
+ * C5 = 1; operation2: /
+ * C7 = 3; types of i and j: long*/
 import java.util.Scanner;
 
 public class Lab1 {
+
+
+
     public static void main(String[] args) {
         double S = 0;
         int C = 416 % 3;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter values:");
-        System.out.print("Enter m: ");
-        int m = scanner.nextInt();
-        System.out.print("Enter n: ");
-        int n = scanner.nextInt();
-        System.out.print("Enter a: ");
-        int a = scanner.nextInt();
-        System.out.print("Enter b: ");
-        int b = scanner.nextInt();
-        if(a != 0){
-            for (long i = a; i < n; i++){
-                for (long j = b; j < m; j++){
-                    S += (i + j)/((double)i/C);
-                }
-            }
-            System.out.println("S = " + S);
+        int m, n, a, b;
+        boolean isSuccesful = false;
+        while(!isSuccesful){
+            try{
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter values:");
+                System.out.print("Enter m: ");
+                m = scanner.nextInt();
+                System.out.print("Enter n: ");
+                n = scanner.nextInt();
+                System.out.print("Enter a: ");
+                a = scanner.nextInt();
+                System.out.print("Enter b: ");
+                b = scanner.nextInt();
+                isSuccesful = true;
+                if(a != 0){
+                    for (long i = a; i < n; i++){
+                        for (long j = b; j < m; j++){
+                            if(i != 0){
+                                S += (i + j)/((double)i/C);
+                            }else {
+                                System.out.println("Error division to 0");
+                                break;
+                            }
+                        }
+                    }
+                    System.out.println("S = " + S);
+                }else {System.out.println("Error division to 0");}
+            }catch (Exception e){System.out.println("Input type is not Integer, try again");}
 
-        }else {System.out.println("Error division to 0");}
-
+        }
     }
+//    private static boolean isInteger(Object o){
+//        return o.getClass().getName().equals("java.lang.Integer");
+//    }
 }
--- src/lab2java/Lab2.java
+++ src/lab2java/Lab2.java
@@ -13,48 +13,59 @@ public class Lab2 {
             System.out.println(Arrays.toString(matrix));
         }
     }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number of rows of matrix: ");
-        int numOfRows = scanner.nextInt();
-        System.out.print("Enter number of columns of matrix: ");
-        int numOfColumns = scanner.nextInt();
-        int[][] B = new int[numOfRows][numOfColumns];
-        for (int i = 0; i < numOfRows; i++){
-            int[] row = new int[numOfColumns];
-            for (int j = 0; j < numOfColumns; j++){
-                Random random = new Random();
-                row[j] = random.nextInt(10);
+        boolean isNotInt = true;
+        int numOfRows;
+        int numOfColumns;
+        while (isNotInt) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter number of rows of matrix: ");
+                numOfRows = scanner.nextInt();
+                System.out.print("Enter number of columns of matrix: ");
+                numOfColumns = scanner.nextInt();
+                isNotInt = false;
+                int[][] B = new int[numOfRows][numOfColumns];
+                for (int i = 0; i < numOfRows; i++) {
+                    int[] row = new int[numOfColumns];
+                    for (int j = 0; j < numOfColumns; j++) {
+                        Random random = new Random();
+                        row[j] = random.nextInt(10);
+                    }
+                    B[i] = row;
+                }
+
+                System.out.println("Generated matrix B:");
+                printMatrix(B);
+                System.out.println();
+
+                int[][] C = new int[numOfColumns][numOfRows];
+                for (int i = 0; i < numOfRows; i++) {
+                    for (int j = 0; j < numOfColumns; j++) {
+                        C[j][i] = B[i][j];
+                    }
+                }
+
+                System.out.println("Matrix C:");
+                printMatrix(C);
+                System.out.println();
+
+                int index = 0;
+                for (int[] row : B) {
+                    int sum = 0;
+                    int count = 0;
+                    for (int col : row) {
+                        sum += col;
+                        count++;
+                    }
+                    index++;
+                    System.out.println("Average value of column " + index + " equals " + (double) sum / count);
+                }
+            } catch (Exception e) {
+                System.out.println("Your input is not Integer");
             }
-            B[i] = row;
-        }
 
-        System.out.println("Generated matrix B:");
-        printMatrix(B);
-        System.out.println();
-
-        int[][] C = new int[numOfColumns][numOfRows];
-        for(int i = 0; i < numOfRows; i++){
-            for(int j = 0; j < numOfColumns; j++){
-                C[j][i] = B[i][j];
-            }
-        }
-
-        System.out.println("Matrix C:");
-        printMatrix(C);
-        System.out.println();
-
-        int index = 0;
-        for(int[] row: B){
-            int sum = 0;
-            int count = 0;
-            for (int col : row){
-                sum += col;
-                count++;
-            }
-            index++;
-            System.out.println("Average value of column " + index + " equals " + (double) sum/count);
         }
     }
-
 }
